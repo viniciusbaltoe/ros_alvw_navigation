336d48554d2e09595fbcb054ba21bdddd11f0d8f
Vinícius Breda Altoé <viniciusbaltoe@gmail.com>
2023-11-22 06:50:38 -0300

    Add analise e cartographer to launch file

--- launch/navigation_all_launch.py
+++ launch/navigation_all_launch.py
@@ -270,9 +270,25 @@ def generate_launch_description():
 
     # Nó para iniciar o nó de navegação
     navigation = Node(
-        package='ros_alvw_navigation',  # Substitua pelo nome do seu pacote
+        package='ros_alvw_navigation',
         executable='navigation',
         name='navigation',
+        output='screen',
+        emulate_tty=True,  # Permite a exibição de mensagens de log no terminal
+    )
+
+    cartographer = Node(
+        package='ros_alvw_navigation',
+        executable='cartographer',
+        name='cartographer',
+        output='screen',
+        emulate_tty=True,  # Permite a exibição de mensagens de log no terminal
+    )
+
+    analise = Node(
+        package='ros_alvw_navigation',
+        executable='analise',
+        name='analise',
         output='screen',
         emulate_tty=True,  # Permite a exibição de mensagens de log no terminal
     )
@@ -310,5 +326,8 @@ def generate_launch_description():
     #ld.add_action(rviz_cmd)
     ld.add_action(bringup_cmd)
     ld.add_action(navigation)
+    ld.add_action(cartographer)
+    ld.add_action(analise)
+
 
     return ld
